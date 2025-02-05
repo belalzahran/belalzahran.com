8abde4a3146b8e3ae77f54d6d7d02f1c3303d7fd
Belal Zahran <belalzahran@gmail.com>
2025-02-05 10:52:30 -0800

    updating resume

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 
-const resumeLink = "https://drive.google.com/file/d/18VEjNLdACHWQe2Ld9YjHpfRLylHJuqkC/view?usp=sharing";
+const resumeLink = "https://drive.google.com/file/d/1CuccOt2QWMhhjsr3Gqy_CNXPxBJO6deE/view?usp=sharing";
