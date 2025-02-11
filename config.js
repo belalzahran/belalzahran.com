2e3339b97937e043e9732e71985fb5c721a2e581
Belal Zahran <belalzahran@gmail.com>
2025-02-10 18:54:02 -0800

    resume link change

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 
-const resumeLink = "https://drive.google.com/file/d/1NmlWFORcoCfJ6I4K6pzrf2Ss-iYqIu-5/view?usp=sharing";
+const resumeLink = "https://raw.githubusercontent.com/belalzahran/Resume/main/resume.pdf";
